6cc0aa1b146083947feb471451d5c50d988a257d
shindevivek68 <43874076+shindevivek68@users.noreply.github.com>
2019-01-16 15:07:47 +0530

    Jet Tv OS files added
    files added

---
+++ JetTvOSSlider/DPadState.swift
@@ -0,0 +1,17 @@
+//
+//  DPadState.swift
+//  TvOSSlider
+//
+//  Created by Vivek Shinde on 16.01.19.
+//  Copyright © 2018 Vivek Shinde. All rights reserved.
+//
+
+import Foundation
+
+enum DPadState {
+    case select
+    case right
+    case left
+    case up
+    case down
+}
---
+++ JetTvOSSlider/JetTvOSSlider.h
@@ -0,0 +1,19 @@
+//
+//  JetTvOSSlider.h
+//  JetTvOSSlider
+//
+//  Created by David Cordero on 23.07.18.
+//  Copyright © 2018 David Cordero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JetTvOSSlider.
+FOUNDATION_EXPORT double JetTvOSSliderVersionNumber;
+
+//! Project version string for JetTvOSSlider.
+FOUNDATION_EXPORT const unsigned char JetTvOSSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JetTvOSSlider/PublicHeader.h>
+
+
